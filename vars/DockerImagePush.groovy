daeccca7d9a2c71c33920a7bc5f922aad943cabe
ambefrankline <www.ambefrank@gmail.com>
2023-10-19 16:49:52 -0400

    added ecr docker image build for ECR

--- vars/DockerImagePush.groovy
+++ vars/DockerImagePush.groovy
@@ -1,14 +1,22 @@
-def call(String project, String ImageTag, String hubUser){
-    withCredentials([usernamePassword(
-        credentialsId: 'docker', 
-        passwordVariable: 'PASS', 
-        usernameVariable: 'USER'
-    )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
-    }
+//def call(String project, String ImageTag, String hubUser){
+//    withCredentials([usernamePassword(
+//        credentialsId: 'docker', 
+//        passwordVariable: 'PASS', 
+//        usernameVariable: 'USER'
+//    )]) {
+//        sh "docker login -u '$USER' -p '$PASS'"
+//    }
 
+//    sh """
+//     docker image push ${hubUser}/${project}:${ImageTag}
+//     docker image push ${hubUser}/${project}:latest
+//    """
+//}
+
+def call(String aws_account-id, String region, String ecr_repoName){
     sh """
-     docker image push ${hubUser}/${project}:${ImageTag}
-     docker image push ${hubUser}/${project}:latest
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account-id}.dkr.ecr.${region}.amazonaws.com
+     docker push ${ecr_repoName}:latest ${aws_account-id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+     
     """
 }
--- vars/DockerImageScan.groovy
+++ vars/DockerImageScan.groovy
@@ -1,8 +1,15 @@
-def call(String project, String ImageTag, String hubUser){
+//def call(String project, String ImageTag, String hubUser){
 
+//    sh """
+
+//     trivy image ${hubUser}/${project}:latest > scan.txt
+//     cat scan.txt
+//    """
+//}
+
+def call(String aws_account-id, String region, String ecr_repoName){
     sh """
-
-     trivy image ${hubUser}/${project}:latest > scan.txt
+     trivy image ${ecr_repoName} ${aws_account-id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
      cat scan.txt
     """
 }
