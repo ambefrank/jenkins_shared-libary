8e13fc8c8b3af8b3a35f753334d64875e9b3c02a
ambefrankline <www.ambefrank@gmail.com>
2023-10-19 17:16:21 -0400

    added ecr docker image build for ECR

--- vars/DockerImageScan.groovy
+++ vars/DockerImageScan.groovy
@@ -9,7 +9,7 @@
 
 def call(String aws_account-id, String region, String ecr_repoName){
     sh """
-     trivy image ${ecr_repoName} ${aws_account-id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+     trivy image ${aws_account-id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
      cat scan.txt
     """
 }
