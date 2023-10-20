b677ffdada7b414a87f5d84961dc38b012a64aae
ambefrankline <www.ambefrank@gmail.com>
2023-10-19 20:05:45 -0400

    added ecr docker image build for ECR

--- vars/DockerImagePush.groovy
+++ vars/DockerImagePush.groovy
@@ -20,10 +20,16 @@ def call(String aws_account_id, String region, String ecr_repoName){
         string(credentialsId: 'AWS_Secret_Access_Key', variable: 'AWS_Secret_Access_Key')
     ]) {
         sh """
-        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-        docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest  
+        aws configure set aws_access_key_id "\$AWS_Access_Key"
+        aws configure set aws_secret_access_key "\$AWS_Secret_Access_Key"
+        docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+          
         """
     }
+
+    sh """
+    docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
 }
 
 
@@ -50,6 +56,6 @@ def call(String aws_account_id, String region, String ecr_repoName){
 //    sh """
 //    aws configure set aws_access_key_id "\$AWS_Access_Key"
 //    aws configure set aws_secret_access_key "\$AWS_Secret_Access_Key"
-//    docker login --username AWS --password-stdin 217340246210.dkr.ecr.us-east-1.amazonaws.com
+//    docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
 //    """
 //}
